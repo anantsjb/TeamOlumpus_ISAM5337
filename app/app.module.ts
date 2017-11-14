01e3c6ce707a832993fd002cdd2a317518200a4b
NagmaM <33336513+NagmaM@users.noreply.github.com>
2017-11-13 19:10:28 -0600

    Add files via upload
    Angular Files for Company Management (no search filter)

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { UserComponent } from './components/user.component';
+import { HomeComponent } from './components/home.component';
+import { UserService } from './Service/user.service';
+import { CompanyService } from './Service/company.service';
+import { CompanyComponent } from './components/company.component';
+
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing, Ng2Bs3ModalModule],
+    declarations: [AppComponent, UserComponent, HomeComponent, CompanyComponent],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }, UserService, CompanyService],
+    bootstrap: [AppComponent]
+
+})
+export class AppModule { }
