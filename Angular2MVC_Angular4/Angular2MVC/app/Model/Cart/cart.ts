24e47868adc1bc04de4fe3b376127890c96c6b89
anantsjb <33336407+anantsjb@users.noreply.github.com>
2017-11-15 03:04:44 -0600

    app js

---
+++ Angular2MVC_Angular4/Angular2MVC/app/Model/Cart/cart.ts
@@ -0,0 +1,10 @@
+﻿export class Cart {
+    Id: number;
+    MaterialCode: string;
+    Description: string;
+    Price: number;
+    CompanyID: number;
+    Quantity: number;
+    UserId: number;
+    ProductID: number;
+} 
