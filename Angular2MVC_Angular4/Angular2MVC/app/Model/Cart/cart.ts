2d564c0df85276e49aeb1d33e098c9336269058c
thunguyen110 <33336398+thunguyen110@users.noreply.github.com>
2017-11-16 01:11:22 -0600

    Add files via upload

---
+++ Angular2MVC_Angular4/Angular2MVC/app/Model/Cart/cart.ts
@@ -0,0 +1,12 @@
+﻿export class Cart {
+    Id: number;
+    MaterialCode: string;
+    Description: string;
+    Price: number;
+    UOM: string;
+    MaterialType: string;
+    CompanyID: number;
+    Quantity: number;
+    UserId: number;
+    ProductID: number;
+} 
